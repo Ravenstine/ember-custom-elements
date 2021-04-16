78c6d48067932aa7147b151f675139abb792bc1a
Ten Bitcomb <tenbitcomb@gmail.com>
2021-04-15 21:06:18 -0700

    Fix dummy-add-on

--- tests/dummy-add-on/index.js
+++ tests/dummy-add-on/index.js
@@ -1,8 +1,21 @@
+/* eslint-disable node/no-extraneous-require */
 'use strict';
+
+const funnel = require('broccoli-funnel');
 
 module.exports = {
   name: require('./package').name,
   isDevelopingAddon() {
     return true;
+  },
+  treeForAddon() {
+    const tree = this._super(...arguments);
+    if (!this.parent || !this.parent.app) {
+      return tree;
+    } else {
+      return funnel(tree, {
+        exclude: [/.*/]
+      });
+    }
   }
 };
