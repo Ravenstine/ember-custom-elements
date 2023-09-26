44dbbb28c582ee37d2a5c1bf8f27e7156afd801b
arthurrsouza <arthur.souza@peek.com>
2023-09-26 10:10:58 -0300

    Fix ember router import

--- addon/lib/route-connections.js
+++ addon/lib/route-connections.js
@@ -1,4 +1,4 @@
-import Ember from './ember-compat';
+import Ember from './ember-compat'
 
 /**
  * This file is a bit of a hack.  What it does is expose access to a
@@ -8,18 +8,24 @@ import Ember from './ember-compat';
  * matching by template name, which may be totally different from the
  * route name if the route is rendering different templates into
  * named routes.
- * 
+ *
  * Fortunately, the internal module for Ember.Route exports its WeakMap
  * for keeping track of what routes are connected to which outlet states.
  */
 
-Ember.__loader.define('ember-custom-elements/route-connections', ['exports', '@ember/-internals/routing/lib/system/route'], function(_exports, _route) {
-  'use strict';
-  Object.defineProperty(_exports, '__esModule', {
-    value: true
-  });
-  _exports.default = _route.ROUTE_CONNECTIONS;
-});
-const { default: ROUTE_CONNECTIONS } = Ember.__loader.require('ember-custom-elements/route-connections');
+Ember.__loader.define(
+  'ember-custom-elements/route-connections',
+  ['exports', '@ember/routing/route'],
+  function (_exports, _route) {
+    'use strict'
+    Object.defineProperty(_exports, '__esModule', {
+      value: true
+    })
+    _exports.default = _route.ROUTE_CONNECTIONS
+  }
+)
+const { default: ROUTE_CONNECTIONS } = Ember.__loader.require(
+  'ember-custom-elements/route-connections'
+)
 
-export default ROUTE_CONNECTIONS;
+export default ROUTE_CONNECTIONS
